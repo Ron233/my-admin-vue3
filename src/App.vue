3e8931e92421e32853f08475c7fb134e43d8472a
wrh <ronwrh@gmail.com>
2020-10-31 15:08:57 +0800

    FIRST

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,63 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+<div v-for="(item,index) in products" :key="item" @click="selectProductFun(index)">{{item}}</div>
+<div>{{product}}</div>
 </template>
+
+<script lang="ts">
+import {
+  ref,
+  reactive,
+  toRefs
+} from 'vue'
+import {
+  createLogger
+} from 'vuex'
+
+interface DataProps {
+  products: string[];
+  product: string;
+  selectProductFun: (index: number) => void
+}
+export default {
+  name: 'App',
+  setup() {
+    // const products = ref([
+    //   '瓜子',
+    //   '矿泉水',
+    //   '八宝粥',
+    //   '脚收一下'
+    // ])
+
+    // const product = ref('未选择')
+
+    // const selectProductFun = (index) => {
+    //   product.value = products.value[index]
+    // }
+    let data: DataProps = reactive({
+      products: [
+        '瓜子',
+        '矿泉水',
+        '八宝粥',
+        '脚收一下'
+      ],
+      product: '未选择',
+      selectProductFun(index: number) {
+        console.log(index)
+        data.product = data.products[index]
+      }
+    })
+    const refData = toRefs(data)
+
+    return {
+      // products,
+      // product,
+      // selectProductFun
+      ...refData
+    }
+  },
+
+}
+</script>
 
 <style>
 #app {
